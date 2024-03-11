76bf7b0b0933f6f0f3df6cd8500331d793692a27
Churchill <churchillrimamskep@gmail.com>
2024-03-11 18:09:46 +0100

    making a react Todo list

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoWrapper from "./components/TodoWrapper";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <div>
+      <TodoWrapper />
+    </div>
+  );
+};
+
+export default App;
