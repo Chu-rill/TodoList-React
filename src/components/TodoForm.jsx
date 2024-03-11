3eae4de5344295b4ac538667ef99c9f3f176207e
Churchill <churchillrimamskep@gmail.com>
2024-03-11 19:18:48 +0100

    added state to the todo form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  }
   return (
-    <form className="TodoForm">
-      <input type="text" className="todo-input" placeholder="Enter task" />
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Enter task"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <button type="submit" className="todo-btn">
         Add Task
       </button>
