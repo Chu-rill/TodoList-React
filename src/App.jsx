90dad897a4419d96bf6b4116c362863d7dd90358
Churchill <churchillrimamskep@gmail.com>
2024-03-12 08:13:05 +0100

    had a problem with the todowrapper component i used curly braces instead of parentheses

--- src/App.jsx
+++ src/App.jsx
@@ -9,5 +9,4 @@ export const App = () => {
     </div>
   );
 };
-
 export default App;
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
-uuidv4();
+import { Todo } from "./Todo";
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
+
   function addTodo(todo) {
     setTodos([
       ...todos,
       { id: uuidv4(), task: todo, completed: false, isEditing: false },
-      console.log(todos),
     ]);
   }
+
   return (
     <div className="TodoWrapper">
       <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
     </div>
   );
 };
+
 export default TodoWrapper;
